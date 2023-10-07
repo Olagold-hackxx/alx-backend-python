625019f6933dcd2622e67bd34af95b5dd11c4460
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-07 06:42:20 -0400

    TYpe annotation

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -5,5 +5,5 @@
 def to_str(n: float) -> str:
     """ function to_str that takes a float n as argument,
     returns the string representation of the float.
-	"""
+    """
     return str(n)
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,7 +4,8 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> str:
-    """function sum_mixed_list which takes a list mxd_lst of integers and floats
+    """function sum_mixed_list which takes a list mxd_lst
+    of integers and floats
     returns their sum as a float"""
     result: float = sum(mxd_lst)
     return result

d2997c45e3b6c8513afd1d8bfb946ad94aa0af8f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-07 06:45:23 -0400

    Type annotation

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,7 +3,7 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> str:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function sum_mixed_list which takes a list mxd_lst
     of integers and floats
     returns their sum as a float"""
--- 0x00-python_variable_annotations/main.py
+++ 0x00-python_variable_annotations/main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
-element_length =  __import__('9-element_length').element_length
+sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
 
-print(element_length.__annotations__)
+print(sum_mixed_list.__annotations__)
+mixed = [5, 4, 3.14, 666, 0.99]
+ans = sum_mixed_list(mixed)
+print(ans == sum(mixed))
+print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))

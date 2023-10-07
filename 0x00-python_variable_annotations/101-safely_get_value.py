7888ad3a035951aba5ab710e01bb2359c1eaa095
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-07 16:02:31 -0400

    Type annotation

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """A type-annotated function safe_first_element"""
 from typing import Optional, Mapping, Any, Union, TypeVar
-
-
 T = TypeVar('T', bound=Mapping)
-def safely_get_value(dct: Mapping, key: Any, default: Optional[T]) -> Union[Any, T]:
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Optional[T])\
+                     -> Union[Any, T]:
     """annotated function"""
     if key in dct:
         return dct[key]

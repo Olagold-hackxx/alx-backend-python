2de519127f12a1beef02656e659263f6b569899b
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-09 22:36:55 -0400

    Asyncio in python

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""An asynchronous coroutine wait_n"""
-from asyncio import create_task, Future
+"""A func task_wait_random """
+from asyncio import create_task, Task
 
 wait_random = __import__(
     '0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Future[float]:
+def task_wait_random(max_delay: int) -> Task[float]:
     """Create a task for wait_random async call"""
     return create_task(wait_random(max_delay))
